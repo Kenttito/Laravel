f83f7027facb5540465fc25814e8b99a577bca0a
Kenneth Ehidiamhen <scottspark@Kenneths-MacBook-Pro.local>
2025-07-09 18:45:47 +0100

    Add /api/health route for Railway health check

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'OK';
+});
+
+Route::get('/api/health', function () {
+    return response('OK', 200);
 });
